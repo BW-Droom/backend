bdbb6f4d168aec60a07ee1d0479c8f641ae6f148
Desiree Morris <desiquinncompany@gmail.com>
2019-11-18 04:04:17 -0500

    set up server to take in port dynamically for deployment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('dotenv').config();
 
 const server = require("./server.js");
+const defaults = require("./config/default.js");
 
-const port = process.env.PORT || 5000; //localhost:5000
+const port = defaults.port; //localhost:5000
 server.listen(port, () => console.log(`\n*** Server Running on Port: ${port} \n ***`));
