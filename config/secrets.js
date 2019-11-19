9bf43514537200892067beb344cd617dc06fe0cb
Desiree Morris <desiquinncompany@gmail.com>
2019-11-19 15:12:35 -0500

    updating secrets file

--- config/secrets.js
+++ config/secrets.js
@@ -1,4 +1,4 @@
 module.exports = {
-    environment: process.env.NODE_ENV || 'development',
+    environment: process.env.DB_ENV || 'development',
     jwtSecret: process.env.JWT_SECRET || 'shhhhh'
 }
