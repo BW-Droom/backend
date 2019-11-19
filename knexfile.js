5dd7f0c72952e26945ef18714d1ab1f5feb83db1
Desiree Morris <desiquinncompany@gmail.com>
2019-11-19 15:23:40 -0500

    removing pool from production

--- knexfile.js
+++ knexfile.js
@@ -40,11 +40,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
     migrations : {
       directory: './data/migrations'
     },
