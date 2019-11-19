79a87dd6dc8e90466403a4629735f60b71ce795a
Desiree Morris <desiquinncompany@gmail.com>
2019-11-19 07:28:26 -0500

    testing a change to dbconfig

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const company = require('./companies/companyDb');
 const server = express();
 
 server.use(express.json());
@@ -12,5 +12,11 @@ server.get('/', (req, res) => {
     res.status(200).json({message: "It's Working!"})
 })
 
+server.get('/api/companies', (req, res) => {
+    company.find()
+        .then( companies => {
+            console.log(companies)
+        })
+})
 
 module.exports = server;
