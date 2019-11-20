d47a13bfba127fbf087323bec6a43749c7fa1a38
Desiree Morris <desiquinncompany@gmail.com>
2019-11-19 19:57:58 -0500

    cors hot-fix

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
 const companyRoutes = require('./api/companies/companyRoutes');
 const seekerRoutes = require('./api/seekers/seekerRoutes');
 const matchRoutes = require('./api/match/matchRoutes');
@@ -7,12 +10,31 @@ const authRoutes = require('./auth/authRoutes')
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+
+const whitelist = ['http://localhost:3000', 'https://droombackend.herokuapp.com/'];
+const corsOptions = {
+    credentials: true,
+    origin: 'http://localhost:3000'
+};
+server.use(cors(corsOptions));
+
 server.use('/api/company', companyRoutes);
 server.use('/api/seeker', seekerRoutes);
 server.use('/api/match', matchRoutes);
 server.use('/auth', authRoutes);
 
-
+const corsOptions = {
+    credentials: true,
+    origin: 'http://localhost:3000'
+    // origin: function (origin, callback) {
+    //     if (whitelist.indexOf(origin) !== -1) {
+    //       return callback(null, true)
+    //     } else {
+    //       return callback(new Error('Not allowed by CORS'))
+    //     }
+    // }
+};
 
 server.get('/', (req, res) => {
 
